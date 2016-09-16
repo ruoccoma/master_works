34106f3c19b6790ffff70fa2b58daa8384ac3f92
Markus Lund <markuslund92@hotmail.com>
2016-09-16 15:18:57 +0200

    Add Keras cifar10_cnn example code

---
+++ helgoy-lund/FlickrDatasetTesting/main.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+from model import generate_model, save_model, load
+from trainer import train_model
+
+from keras.datasets import cifar10
+from keras.layers import Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Dense, Dropout
+from keras.models import Sequential
+from keras.models import model_from_yaml
+from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import np_utils
+
+nb_classes = 10
+
+# the data, shuffled and split between train and test sets
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+model = generate_model(X_train.shape[1:], nb_classes)
+train_model(model, X_train, X_test, Y_train, Y_test)
+save_model(model)
+
+model = load()
+model = generate_model(X_train.shape[1:], nb_classes)
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
+
+print(model.evaluate(X_train, Y_train, batch_size))
---
+++ helgoy-lund/FlickrDatasetTesting/model.py
@@ -0,0 +1,65 @@
+from __future__ import print_function
+
+from keras.datasets import cifar10
+from keras.layers import Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Dense, Dropout
+from keras.models import Sequential
+from keras.models import model_from_yaml
+from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import np_utils
+
+yaml_model_filename = "model.yml"
+model_weights_filename = "weights"
+
+def generate_model(input_shape, nb_classes):
+	model = Sequential()
+	model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=input_shape))
+	model.add(Activation('relu'))
+	model.add(Convolution2D(32, 3, 3))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.25))
+	model.add(Convolution2D(64, 3, 3, border_mode='same'))
+	model.add(Activation('relu'))
+	model.add(Convolution2D(64, 3, 3))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.25))
+	model.add(Flatten())
+	model.add(Dense(512))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.5))
+	model.add(Dense(nb_classes))
+	model.add(Activation('softmax'))
+	# let's train the model using SGD + momentum (how original).
+	sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+	model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+	return model
+
+
+# save/load models in keras http://machinelearningmastery.com/save-load-keras-deep-learning-models/
+def load():
+	# load YAML and create model
+	yaml_file = open(yaml_model_filename, 'r')
+	loaded_model_yaml = yaml_file.read()
+	yaml_file.close()
+	loaded_model = model_from_yaml(loaded_model_yaml)
+	# load weights into new model
+	loaded_model.load_weights(model_weights_filename)
+	print("Loaded model from disk")
+	return loaded_model
+
+
+# yaml_filename = "model.yaml"
+# h5_weights_filename = "model.h5"
+def save_model(model):
+	# serialize model to YAML
+	model_yaml = model.to_yaml()
+	with open(yaml_model_filename, "w") as yaml_file:
+		yaml_file.write(model_yaml)
+	# serialize weights to HDF5
+	model.save_weights(model_weights_filename)
+	print("Saved model to disk")
