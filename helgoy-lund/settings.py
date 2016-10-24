3142b683f8058033a686d6ac01a0faec48cc56a4
dagih <dihelgoy@gmail.com>
2016-10-24 15:33:13 +0200

    Fix image vector path in settings Add normalization of image vectors

--- helgoy-lund/settings.py
+++ helgoy-lund/settings.py
@@ -5,9 +5,10 @@ DB_FILE_PATH = ROOT_DIR + "/data/databases/sqlite/database.db"
 
 CNN_NAME = "inception"
 WORD_EMBEDDING_METHOD = "word2vec"
+IMAGE_DIR = ROOT_DIR + "/data/datasets/Flickr_8k/images/"
 
 # Word2Vec
-WORD_EMBEDDING_DIMENSION = 300
+WORD_EMBEDDING_DIMENSION = 128
 WORD_EMBEDDING_DIR = ROOT_DIR + "/models/word2vec/embeddings/"
 WORD_FILEPATH = ROOT_DIR + "/data/datasets/Flickr_8k/Flickr8k.token.txt"
 
