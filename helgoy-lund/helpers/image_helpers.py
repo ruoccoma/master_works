8c435052ae5f14fd67e7556f914c8b51d1deb846
Markus Lund <markuslund92@hotmail.com>
2016-10-31 15:47:56 +0100

    Support finding images for new captions

--- helgoy-lund/helpers/image_helpers.py
+++ helgoy-lund/helpers/image_helpers.py
@@ -3,10 +3,15 @@ import settings
 
 
 def show_image(file, title):
-	img = Image.open(file)
-	draw = ImageDraw.Draw(img)
-	font = ImageFont.truetype(settings.RES_DIR + "Montserrat-Bold.ttf", 18)
-	draw.text((0, 0), title, (0, 0, 0), font=font)
-	draw.text((1, 1), title, (255, 255, 255), font=font)
-	img.show(title=title)
+	try:
+		img = Image.open(file)
+		draw = ImageDraw.Draw(img)
+		font = ImageFont.truetype(settings.RES_DIR + "Montserrat-Bold.ttf", 18)
+		draw.text((0, 0), title, (0, 0, 0), font=font)
+		draw.text((1, 1), title, (255, 255, 255), font=font)
+		img.show(title=title)
+	except Exception as e:
+		print("filename:", file)
+		print(e)
+
 
--- helgoy-lund/models/word2visualvec/main.py
+++ helgoy-lund/models/word2visualvec/main.py
@@ -1,3 +1,5 @@
+from random import randint
+
 import numpy
 import theano
 from sklearn.metrics import mean_squared_error
@@ -17,8 +19,8 @@ theano.config.openmp = True
 import feedforward_keras
 # Settings
 SAVE_MODEL = True
-LOAD_MODEL = False
-USER_INPUT = False
+LOAD_MODEL = True
+PREDICT_NEW = False
 MODELS = [multibranch_keras, feedforward_keras]
 MODEL = MODELS[0]
 MODEL_SUFFIX = "-caption-model-10-epochs"
@@ -34,34 +36,23 @@ def word2visualvec_main():
 			save_model_to_file(model, MODEL.__name__)
 		prediction_model = MODEL.generate_prediction_model(model)
 
-	test_model(prediction_model)
-	# predict(prediction_model)
+	if PREDICT_NEW:
+		predict(prediction_model)
+	else:
+		test_model(prediction_model)
 
 
 def save_model_to_file(model, name):
 	name += MODEL_SUFFIX
-	# serialize model to JSON
-	# model_json = model.to_json()
-	# with open("stored_models/" + name + ".json", "w") as json_file:
-	# 	json_file.write(model_json)
-	# serialize weights to HDF5
 	model.save_weights("stored_models/" + name + ".h5")
 	print("Saved model \"%s\" to disk" % name)
 
 
 def load_model(name):
 	name += MODEL_SUFFIX
-	# load json and create model
-	# json_file = open("stored_models/" + name + '.json', 'r')
-	# loaded_model_json = json_file.read()
-	# json_file.close()
-	# loaded_model = model_from_json(loaded_model_json)
-	# load weights into new model
 
 	loaded_model = MODEL.get_model()
 	loaded_model.load_weights("stored_models/" + name + ".h5")
-
-	weights = loaded_model.get_weights()
 
 	print("Loaded model \"%s\" from disk" % name)
 
@@ -89,58 +80,32 @@ def convert_captions_to_vectors(queries):
 def predict(model):
 	captions = []
 	user_provided_caption = " "
-	# while 1:
-	# 	user_provided_caption = input("EXIT WITH EMPTY - Enter caption: ")
-	# 	if user_provided_caption == "":
-	# 		break
-	# 	else:
-	# 		captions.append(user_provided_caption)
-	# samples = convert_captions_to_vectors(captions)
-	samples = convert_captions_to_vectors(
-		["A boy is sliding towards a base while the baseman is waiting for the ball in a baseball game ."])
+	while 1:
+		user_provided_caption = input("EXIT WITH EMPTY - Enter caption: ")
+		if user_provided_caption == "":
+			break
+		else:
+			captions.append(user_provided_caption)
+	samples = convert_captions_to_vectors(captions)
 	for i in range(len(samples)):
 		correct_caption_vector_list = samples[i:i + 1]
-		correct_caption_vector = correct_caption_vector_list[0]
 
 		predicted_image_vector = model.predict(correct_caption_vector_list)[0]
 
-		image_vector_pairs = fetch_all_image_vector_pairs()
-		first_image_vector = image_vector_pairs[0][1]
-		first_image_filename = image_vector_pairs[0][0]
-		first_image_mse = compare_vectors(predicted_image_vector, first_image_vector)
-		# print("first image mse:", first_image_mse, "predicted vector:", predicted_image_vector)
-
-		best_image_vector_mse_list = [0 for i in range(5)]
-		best_image_vector_name_list = ["" for i in range(5)]
-		best_image_vector_list = [[] for i in range(5)]
-
-		best_image_vector_mse_list = insert_and_remove_last(0, best_image_vector_mse_list, first_image_mse)
-		best_image_vector_name_list = insert_and_remove_last(0, best_image_vector_name_list, first_image_filename)
-		best_image_vector_list = insert_and_remove_last(0, best_image_vector_list, first_image_vector)
-
-		for temp_image_name, temp_image_vector in image_vector_pairs:
-			temp_image_mse = compare_vectors(predicted_image_vector, temp_image_vector)
-			for index in range(len(best_image_vector_list)):
-				if temp_image_mse < best_image_vector_mse_list[index]:
-					best_image_vector_mse_list = insert_and_remove_last(index, best_image_vector_mse_list,
-					                                                    temp_image_mse)
-					best_image_vector_name_list = insert_and_remove_last(index, best_image_vector_name_list,
-					                                                     temp_image_name)
-					best_image_vector_list = insert_and_remove_last(index, best_image_vector_list, temp_image_vector)
-					break
-		print("Result:")
-		for i in range(len(best_image_vector_mse_list)):
+		best_image_vector_name_list = find_n_most_similar(predicted_image_vector)
+		print("Result for %s:" % captions[i])
+		for i in range(len(best_image_vector_name_list)):
 			filename = best_image_vector_name_list[i]
 			show_image(settings.IMAGE_DIR + filename, str(i + 1) + "-" + filename)
 			print(i + 1, filename)
 		print("")
 
+
 def test_model(model):
 	test_size = 1
 	all_caption_vectors = fetch_test_captions_vectors()
-	# numpy.random.shuffle(all_caption_vectors)
-	# start = randint(0, len(all_caption_vectors) - test_size)
-	start = 0
+	numpy.random.shuffle(all_caption_vectors)
+	start = randint(0, len(all_caption_vectors) - test_size)
 	samples = all_caption_vectors[start:start + test_size]
 	print("\nRESULTS")
 	for i in range(len(samples)):
@@ -152,37 +117,15 @@ def test_model(model):
 
 		predicted_image_vector = model.predict(correct_caption_vector_list)[0]
 
-		image_vector_pairs = fetch_all_image_vector_pairs()
-		first_image_vector = image_vector_pairs[0][1]
-		first_image_filename = image_vector_pairs[0][0]
-		first_image_mse = compare_vectors(predicted_image_vector, first_image_vector)
-		# print("first image mse:", first_image_mse, "predicted vector:", predicted_image_vector)
+		best_image_vector_name_list = find_n_most_similar(predicted_image_vector)
 
-		best_image_vector_mse_list = [0 for i in range(5)]
-		best_image_vector_name_list = ["" for i in range(5)]
-		best_image_vector_list = [[] for i in range(5)]
-
-		best_image_vector_mse_list = insert_and_remove_last(0, best_image_vector_mse_list, first_image_mse)
-		best_image_vector_name_list = insert_and_remove_last(0, best_image_vector_name_list, first_image_filename)
-		best_image_vector_list = insert_and_remove_last(0, best_image_vector_list, first_image_vector)
-
-		for temp_image_name, temp_image_vector in image_vector_pairs:
-			temp_image_mse = compare_vectors(predicted_image_vector, temp_image_vector)
-			for index in range(len(best_image_vector_list)):
-				if temp_image_mse < best_image_vector_mse_list[index]:
-					best_image_vector_mse_list = insert_and_remove_last(index, best_image_vector_mse_list,
-					                                                    temp_image_mse)
-					best_image_vector_name_list = insert_and_remove_last(index, best_image_vector_name_list,
-					                                                     temp_image_name)
-					best_image_vector_list = insert_and_remove_last(index, best_image_vector_list, temp_image_vector)
-					break
 		print("")
 		print("Correct caption:\t", correct_image_caption)
 		print("")
 		print("Correct filename:\t", correct_image_filename)
 		print("")
 		print("Result:")
-		for i in range(len(best_image_vector_mse_list)):
+		for i in range(len(best_image_vector_name_list)):
 			filename = best_image_vector_name_list[i]
 			show_image(settings.IMAGE_DIR + filename, str(i + 1) + "-" + filename)
 			print(i + 1, filename)
@@ -190,8 +133,36 @@ def test_model(model):
 		show_image(settings.IMAGE_DIR + correct_image_filename, "QUERY: " + correct_image_caption)
 
 
+def find_n_most_similar(predicted_image_vector):
+	image_vector_pairs = fetch_all_image_vector_pairs()
+
+	first_image_vector = image_vector_pairs[0][1]
+	first_image_filename = image_vector_pairs[0][0]
+	first_image_mse = compare_vectors(predicted_image_vector, first_image_vector)
+
+	best_image_vector_mse_list = [0 for i in range(5)]
+	best_image_vector_name_list = ["" for i in range(5)]
+	best_image_vector_list = [[] for i in range(5)]
+
+	best_image_vector_mse_list = insert_and_remove_last(0, best_image_vector_mse_list, first_image_mse)
+	best_image_vector_name_list = insert_and_remove_last(0, best_image_vector_name_list, first_image_filename)
+	best_image_vector_list = insert_and_remove_last(0, best_image_vector_list, first_image_vector)
+
+	for temp_image_name, temp_image_vector in image_vector_pairs:
+		temp_image_mse = compare_vectors(predicted_image_vector, temp_image_vector)
+		for index in range(len(best_image_vector_list)):
+			if temp_image_mse < best_image_vector_mse_list[index]:
+				best_image_vector_mse_list = insert_and_remove_last(index, best_image_vector_mse_list,
+				                                                    temp_image_mse)
+				best_image_vector_name_list = insert_and_remove_last(index, best_image_vector_name_list,
+				                                                     temp_image_name)
+				best_image_vector_list = insert_and_remove_last(index, best_image_vector_list, temp_image_vector)
+				break
+	return best_image_vector_name_list
+
+
 def fetch_test_captions_vectors():
-	data_x, data_y = structure_and_store_embeddings(50)
+	data_x, data_y = structure_and_store_embeddings()
 	training_test_ratio = 0.8
 	_, test_x = split_list(data_x, training_test_ratio)
 	return numpy.asarray(data_x)
