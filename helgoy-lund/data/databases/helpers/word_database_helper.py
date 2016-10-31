d1c1fbeb5d4c23d5c4e79f8155ea7293a579f9ce
Dag Inge Helgøy <dihelgoy@gmail.com>
2016-10-31 12:30:03 +0100

    Fix naming error

--- helgoy-lund/data/databases/helpers/word_database_helper.py
+++ helgoy-lund/data/databases/helpers/word_database_helper.py
@@ -2,7 +2,7 @@
 import sqlite_wrapper as db_wrapper
 
 
-def save_caption_vector(word_text, word_vector):
+def save_word_vector(word_text, word_vector):
     return db_wrapper.db_insert_word_vector(word_text, word_vector)
 
 def fetch_all_word_vectors():
