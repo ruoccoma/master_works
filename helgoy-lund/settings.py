dbd093d548f2d1dda39315d9311e6fffdd911ea0
Markus Lund <markuslund92@hotmail.com>
2016-11-08 16:21:28 +0100

    Change settings to 30k

--- helgoy-lund/settings.py
+++ helgoy-lund/settings.py
@@ -19,7 +19,7 @@ DB_FILE_PATH = ROOT_DIR + "/data/databases/sqlite/database.db"
 
 CNN_NAME = "inception"
 WORD_EMBEDDING_METHOD = "word2vec"
-DATASET = "Flickr8k"
+DATASET = "Flickr30k"
 RES_DIR = ROOT_DIR + "res/"
 IMAGE_DIR = ROOT_DIR + "data/datasets/" + DATASET + "/images/"
 CREATE_NEGATIVE_EXAMPLES = False
