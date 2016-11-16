1bc29428dfb6c74edfe38ad2f00e635ca2ff6521
Dag Inge Helgøy <dihelgoy@gmail.com>
2016-11-16 14:01:48 +0100

    Add glove

---
+++ helgoy-lund/models/word_embedding/glove/glove.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from word_database_helper import save_word_vector_tuple
+
+
+def run_glove():
+	with open("glove.6B.300d.txt") as word_embedding:
+		print("Saving word embeddings to database...")
+		words = []
+		for line in word_embedding.readlines():
+			line = (line.strip()).split(" ")
+			word = line.pop(0)
+			line = map(float, line)
+			words.append((word, np.asarray(line)))
+		save_word_vector_tuple(words)
+
+
+if __name__ == "__main__":
+	run_glove()
