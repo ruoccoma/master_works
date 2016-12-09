fixed length of masking

skrede/code/yoochoose/rnn_model.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 import pickle
-import numpy as np
 
 location  = '/home/ole/recSys-pro/yoochoose/'
 n_hidden  = 100
@@ -13,6 +12,7 @@ k                   = meta_data['k']
 train_files         = meta_data['train_files']
 n_classes           = meta_data['n_classes']
 batch_size          = meta_data['batch_size']
+max_epochs          = meta_data['max_epochs']
 n_readers            = 8
 n_preprocess_threads = 8
 pickle.dump(meta_data, open(meta_pickle, 'wb'))
@@ -57,9 +57,9 @@ for thread_id in range(n_preprocess_threads):
     targets  = example[2:]
     
     # Create a list with the value of sequence_length. E.g. 5 -> [5]
-    input_length = tf.expand_dims(tf.sub(sequence_length, 1), 0)
+    input_length = tf.expand_dims(sequence_length, 0)
     # Use this to mask out outputs from the rnn, so we don't train on padding
-    indicator = tf.ones(input_length, dtype=tf.int32)
+    indicator = tf.reshape(tf.sequence_mask(input_length, maxlen=max_sequence_length-1, dtype=tf.int32), [-1])
 
     parsed_examples.append([sequence_length, features, targets, indicator])
 
@@ -68,7 +68,7 @@ session_length, x_unparsed, y_unparsed, mask = tf.train.batch_join(
         parsed_examples, 
         batch_size=batch_size,
         capacity=2*n_preprocess_threads*batch_size,
-        dynamic_pad=True)
+        dynamic_pad=False)
 
 
 # Parse the examples in a batch
@@ -100,20 +100,22 @@ output = tf.nn.dropout(output, keep_prob)
 # The models prediction
 prediction = tf.matmul(output, layer['weights'], name="prediction")# + layer['biases'] TODO
 
-mask_weights = tf.to_float(mask)
-
+# Reshape the prediction to fit in the loss function
 prediction_reshaped = tf.reshape(prediction, [batch_size, max_sequence_length-1, n_classes])
 
-# Reduce sum, since average divides by max_length, which is often wrong
+# Compute losses
 losses = tf.nn.sparse_softmax_cross_entropy_with_logits(prediction_reshaped, y, name="losses")
 
+# Use the masks to filter out loss on padded values that appears in shorter sequences
+mask_weights = tf.to_float(mask)
 batch_loss = tf.div(tf.reduce_sum(tf.mul(losses, mask_weights)),
         tf.reduce_sum(mask_weights),
         name="batch_loss")
 
-# The training 'function'
+# The optimizer for training
 optimizer = tf.train.AdamOptimizer(name="optimizer").minimize(batch_loss)
 
 # Top k predictions for testing
 top_pred_vals, top_pred_indices = tf.nn.top_k(prediction, k=k, sorted=True, name="topPredictions")
 
+top_k_preds = tf.reshape(top_pred_indices, [batch_size, max_sequence_length-1, k])

skrede/code/yoochoose/run_training.py
@@ -1,41 +1,95 @@
 import pickle
 import time
+import numpy as np
+from data_handler import *
 
 # Specify stuff here
 batch_size = 200
-max_epochs = 10
+max_epochs = 50
 location = '/home/ole/recSys-pro/yoochoose/'
+test_file = location + 'rsc15_test_ole.csv'
 
 # Load and store some parameters
 meta_pickle = location + 'meta.pickle'
 meta_data   = pickle.load( open(meta_pickle, 'rb'))
 meta_data['batch_size'] = batch_size
+meta_data['max_epochs'] = max_epochs
+
+n_classes           = meta_data['n_classes']
+n_training_examples = meta_data['n_training_examples']
+k                   = meta_data['k']
 pickle.dump(meta_data, open(meta_pickle, 'wb'))
+
+test_data = load_data_set(test_file)
 
 # Create the model
 from rnn_model import *
 
+print('')
+print("model created, starting training with the following parameters")
+print("  max_epochs:", max_epochs, "| batch_size:", batch_size, "| data files from", location)
+print('')
+
 # Run training
+init_op = tf.group(tf.initialize_all_variables(), tf.initialize_local_variables())
 with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-    epoch_time = time.time()
-    batch_time = time.time()
-    
-    for i in range(1926):
-        batch_cost=0
-        
-        _, c = sess.run([optimizer, batch_loss], feed_dict={keep_prob:0.5})
-        #print(ait.shape)
-        batch_cost += c
+    for e in range(max_epochs):
+        epoch_time = time.time()
+        epoch_loss = 0
+    	
+        num_batches = int(n_training_examples/batch_size)
+        print("Num batches:", str(num_batches), "| progress: ", end='')
+        progres_part = int(num_batches/10)
+        next_land_mark = progres_part
+        lm_t = time.time()
 
-        if i%10==0:
-            print("10 batches time", str(time.time()-batch_time))
-            batch_time = time.time()
-            print("  cost", str(batch_cost))
+        for b in range(num_batches):
+            _, l = sess.run([optimizer, batch_loss], feed_dict={keep_prob:0.5})
+            epoch_loss += l
+            if b==next_land_mark:
+                print(str(b), str(time.time()-lm_t), end=' ')
+                lm_t = time.time()
+                next_land_mark += progres_part
 
+        print("epoch #"+str(e),"finished in", str(time.time()-epoch_time), "s")
+        print("  epoch loss", epoch_loss)
 
-    print("epoch time", str(time.time()-epoch_time))
+        print("testing...")
+        bt = time.time()
+        test_batch = test_data[:200]
+        sl = [e[0] for e in test_batch]
+        xu = [e[1] for e in test_batch]
+        yu = [e[2] for e in test_batch]
+        m = [e[3] for e in test_batch]
+        print("  test_batch created in", str(time.time()-bt))
+
+        preds = sess.run(top_k_preds, feed_dict={keep_prob:1.0, session_length:sl, x_unparsed:xu, mask:m})
+
+        # Calculate accuracy
+        recall, mrr = 0.0, 0.0
+        evaluation_count = 0
+        for batch_index in range(preds.shape[0]):
+            pred_sequence = preds[batch_index]
+            target_sequence = yu[batch_index]
+
+            for i in range(sl[batch_index]):
+                target_item = target_sequence[i]
+                k_predictions = pred_sequence[i]
+
+                if target_item in k_predictions:
+                    recall += 1
+                    rank = np.nonzero(k_predictions == target_item)[0][0]+1
+                    mrr += 1.0/rank
+                
+                evaluation_count += 1
+        recall = recall/evaluation_count
+        mrr = mrr/evaluation_count
+
+        print("  Recall@"+str(k)+":", recall)
+        print("  MRR@"+str(k)+":", mrr)
+
     coord.request_stop()
     coord.join(threads)